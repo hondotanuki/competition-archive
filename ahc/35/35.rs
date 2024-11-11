368be159f34987db9282b40858a8a84969a8592b
hondotanuki <yuuki@yuukiPC>
2024-11-11 18:23:35 +0900

    関数を入れ替えただけ

--- ahc/35/35.rs
+++ ahc/35/35.rs
@@ -36,83 +36,6 @@ fn calc_score(tanes: &Vec<Vec<i32>>) -> i32 {
     tanes.iter().map(|tane| tane.iter().sum()).max().unwrap()
 }
 
-fn generate_new_tanes(
-    tanes: &Vec<Vec<i32>>,
-    field: &Vec<Vec<usize>>,
-    rng: &mut impl Rng,
-) -> Vec<Vec<i32>> {
-    let mut new_tanes = Vec::new();
-    let mut visited = HashSet::new();
-    for i in 0..N {
-        for j in 0..N {
-            for &(di, dj) in &DIJ {
-                let ni = i as i32 + di;
-                let nj = j as i32 + dj;
-                if ni < 0 || nj < 0 || ni >= N as i32 || nj >= N as i32 {
-                    continue;
-                }
-                let ni = ni as usize;
-                let nj = nj as usize;
-                let idx1 = field[i][j];
-                let idx2 = field[ni][nj];
-                let pair = if idx1 < idx2 {
-                    (idx1, idx2)
-                } else {
-                    (idx2, idx1)
-                };
-                if visited.contains(&pair) {
-                    continue;
-                }
-                visited.insert(pair);
-
-                let mut new_tane = Vec::with_capacity(M);
-                for k in 0..M {
-                    let value = if rng.gen_bool(0.5) {
-                        tanes[idx1][k]
-                    } else {
-                        tanes[idx2][k]
-                    };
-                    new_tane.push(value);
-                }
-                new_tanes.push(new_tane);
-            }
-        }
-    }
-    new_tanes
-}
-
-fn swap_field_elements(max_field: &mut Vec<Vec<usize>>, rng: &mut impl Rng) -> Vec<Vec<usize>> {
-    let mut swap = Vec::new();
-    let q = rand::thread_rng().gen_range(0..3);
-    if q == 1 {
-        while swap.len() < 2 {
-            let i = rng.gen_range(0..N);
-            let j = rng.gen_range(0..N);
-            if EDGES.contains(&(i, j)) || CORNERS.contains(&(i, j)) {
-                continue;
-            }
-            swap.push((i, j));
-        }
-    } else if q == 1 {
-        swap = EDGES
-            .choose_multiple(&mut rand::thread_rng(), 2)
-            .cloned()
-            .collect();
-    } else if q == 2 {
-        swap = CORNERS
-            .choose_multiple(&mut rand::thread_rng(), 2)
-            .cloned()
-            .collect();
-    }
-    let mut swapped_field = max_field.clone();
-    if let [(i1, j1), (i2, j2)] = swap.as_slice() {
-        let tmp = swapped_field[*i1][*j1];
-        swapped_field[*i1][*j1] = swapped_field[*i2][*j2];
-        swapped_field[*i2][*j2] = tmp;
-    }
-    swapped_field
-}
-
 fn zscore_sorted_idx(tanes: &Vec<Vec<i32>>) -> VecDeque<usize> {
     let mut vec_j = vec![Vec::with_capacity(NX); M];
     for i in 0..NX {
@@ -174,6 +97,83 @@ fn create_field(z_sorted_idx: &mut VecDeque<usize>) -> Vec<Vec<usize>> {
         field[i][j] = z_sorted_idx.pop_back().unwrap();
     }
     field
+}
+
+fn swap_field_elements(max_field: &mut Vec<Vec<usize>>, rng: &mut impl Rng) -> Vec<Vec<usize>> {
+    let mut swap = Vec::new();
+    let q = rand::thread_rng().gen_range(0..3);
+    if q == 1 {
+        while swap.len() < 2 {
+            let i = rng.gen_range(0..N);
+            let j = rng.gen_range(0..N);
+            if EDGES.contains(&(i, j)) || CORNERS.contains(&(i, j)) {
+                continue;
+            }
+            swap.push((i, j));
+        }
+    } else if q == 1 {
+        swap = EDGES
+            .choose_multiple(&mut rand::thread_rng(), 2)
+            .cloned()
+            .collect();
+    } else if q == 2 {
+        swap = CORNERS
+            .choose_multiple(&mut rand::thread_rng(), 2)
+            .cloned()
+            .collect();
+    }
+    let mut swapped_field = max_field.clone();
+    if let [(i1, j1), (i2, j2)] = swap.as_slice() {
+        let tmp = swapped_field[*i1][*j1];
+        swapped_field[*i1][*j1] = swapped_field[*i2][*j2];
+        swapped_field[*i2][*j2] = tmp;
+    }
+    swapped_field
+}
+
+fn generate_new_tanes(
+    tanes: &Vec<Vec<i32>>,
+    field: &Vec<Vec<usize>>,
+    rng: &mut impl Rng,
+) -> Vec<Vec<i32>> {
+    let mut new_tanes = Vec::new();
+    let mut visited = HashSet::new();
+    for i in 0..N {
+        for j in 0..N {
+            for &(di, dj) in &DIJ {
+                let ni = i as i32 + di;
+                let nj = j as i32 + dj;
+                if ni < 0 || nj < 0 || ni >= N as i32 || nj >= N as i32 {
+                    continue;
+                }
+                let ni = ni as usize;
+                let nj = nj as usize;
+                let idx1 = field[i][j];
+                let idx2 = field[ni][nj];
+                let pair = if idx1 < idx2 {
+                    (idx1, idx2)
+                } else {
+                    (idx2, idx1)
+                };
+                if visited.contains(&pair) {
+                    continue;
+                }
+                visited.insert(pair);
+
+                let mut new_tane = Vec::with_capacity(M);
+                for k in 0..M {
+                    let value = if rng.gen_bool(0.5) {
+                        tanes[idx1][k]
+                    } else {
+                        tanes[idx2][k]
+                    };
+                    new_tane.push(value);
+                }
+                new_tanes.push(new_tane);
+            }
+        }
+    }
+    new_tanes
 }
 
 fn main() {
